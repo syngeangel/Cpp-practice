462f7889cf7b4d77839c33e02c7f76471cd29b88
syngeangel <119157472@qq.com>
2015-07-30 22:15:21 +0800

    我提交

---
+++ PKU/Car.cpp
@@ -0,0 +1,35 @@
+class CTyre{
+private:
+	int radius;
+	int width;
+public:
+	CTyre(int r, int w):radius(r), width(w){}
+};
+
+class CEngine{
+};
+
+class CCar
+{
+public:
+	CCar(int p,int tr,int tw);
+	~CCar();
+
+private:
+	int price;
+	CTyre tyre;
+	CEngine engine;
+};
+
+CCar::CCar(int p, int tr, int w) :price(p), tyre(tr,w)
+{
+}
+
+CCar::~CCar()
+{
+}
+
+int main(){
+	CCar car(20000, 17, 225);
+	return 0;
+}
